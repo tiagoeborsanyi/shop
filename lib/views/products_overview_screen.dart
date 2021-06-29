2e755af0564b65c293f7044e7aa9c9b09f9d0b05
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-29 16:00:21 -0300

    created simple component productsOverview

---
+++ lib/views/products_overview_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../data/dummy_data.dart';
+import '../models/product.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = DUMMY_PRODUCTS;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
