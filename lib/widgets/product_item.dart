9d8a4109fddfe065510a12a7f2bf89cd66d0a3d6
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-29 16:11:03 -0300

    created simple productItem component

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(product.imageUrl),
+    );
+  }
+}
