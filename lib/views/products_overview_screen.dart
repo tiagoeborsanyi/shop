603bf8251090e28315cfc1f101b8b1ac7aab8402
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-29 16:08:13 -0300

    create simple gridView productsOverview

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -8,6 +8,21 @@ class ProductOverviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
   }
 }
