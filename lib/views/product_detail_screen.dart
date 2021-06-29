28beeefdb9e3a9a0c452f96950c39aff9e450428
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-06-29 16:34:28 -0300

    created productDetail component

---
+++ lib/views/product_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  final Product product;
+
+  const ProductDetailScreen(this.product);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+      ),
+    );
+  }
+}
