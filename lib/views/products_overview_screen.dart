0053d2d13b2bd9a70e51999a90a222d9952894c2
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-05 15:39:40 -0300

    add popupMenu on appBar home

--- lib/views/products_overview_screen.dart
+++ lib/views/products_overview_screen.dart
@@ -2,12 +2,35 @@ import 'package:flutter/material.dart';
 
 import '../widgets/product_grid.dart';
 
+enum FilterOptions {
+  Favorite,
+  All,
+}
+
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Minha Loja'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              print(selectedValue);
+            },
+            icon: Icon(Icons.more_vert_rounded),
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Somente Favoritos'),
+                value: FilterOptions.Favorite,
+              ),
+              PopupMenuItem(
+                child: Text('Todos'),
+                value: FilterOptions.All,
+              ),
+            ],
+          )
+        ],
       ),
       body: ProductGrid(),
     );
