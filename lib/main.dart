ca28cbdddd352dc16015661440fa85d9e2949a30
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-05 16:56:25 -0300

    remove banner debug on appBar

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Minha Loja',
         theme: ThemeData(
           primarySwatch: Colors.purple,
