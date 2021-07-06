7103079528067707679098190cca438b5c47a16a
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-06 17:22:00 -0300

    created simple component orderWidget

---
+++ lib/widgets/order_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../providers/orders.dart';
+
+class OrderWidget extends StatelessWidget {
+  final Order order;
+
+  OrderWidget(this.order);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: ListTile(
+        title: Text('R\$ ${order.total}'),
+        subtitle: Text(DateFormat('dd/MM/yyyy hh:mm').format(order.date)),
+        trailing: IconButton(
+          icon: Icon(Icons.expand_more),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
