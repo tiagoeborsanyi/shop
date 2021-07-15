ea83c7737b8f5b28bb98b1e7da967f15ad31ab15
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-15 12:04:00 -0300

    Add listView.builder on productsScreen component

--- lib/views/products_screen.dart
+++ lib/views/products_screen.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../providers/products.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final products = Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Produtos'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: products.itemsCount,
+          itemBuilder: (ctx, i) => Text('TESTE'),
+        ),
       ),
     );
   }
