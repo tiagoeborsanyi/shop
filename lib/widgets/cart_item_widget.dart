515453815d1fe8ceaf0321b53437e04f06b59b06
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-15 10:47:23 -0300

    create alertDialog for delete item of cart

--- lib/widgets/cart_item_widget.dart
+++ lib/widgets/cart_item_widget.dart
@@ -28,6 +28,28 @@ class CartItemWidget extends StatelessWidget {
           margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         ),
         direction: DismissDirection.endToStart,
+        confirmDismiss: (_) {
+          return showDialog(
+              context: context,
+              builder: (ctx) => AlertDialog(
+                    title: Text('Tem certeza?'),
+                    content: Text('Quer remover este item.'),
+                    actions: [
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                        },
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () {
+                          Navigator.of(ctx).pop(true);
+                        },
+                      ),
+                    ],
+                  ));
+        },
         onDismissed: (_) {
           Provider.of<Cart>(context, listen: false)
               .removeItem(cartItem.productId);
