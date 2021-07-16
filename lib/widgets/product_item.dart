1ae7cfeb582f06ecb969211cf14f43a91fa95875
Tiago Emerick <tiagoeborsanyi@gmail.com>
2021-07-16 20:45:07 -0300

    create showDialog for delete product

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -34,8 +34,31 @@ class ProductItem extends StatelessWidget {
               icon: Icon(Icons.delete),
               color: Theme.of(context).errorColor,
               onPressed: () {
-                Provider.of<Products>(context, listen: false)
-                    .deleteProduct(product.id);
+                showDialog(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: Text('Tem certeza disso?'),
+                    actions: [
+                      TextButton(
+                        child: Text('Não'),
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                        },
+                      ),
+                      TextButton(
+                        child: Text('Sim'),
+                        onPressed: () {
+                          Navigator.of(ctx).pop(true);
+                        },
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  // O pop() false nao entra dentro desse then, ele só fecha a janela e nao faz nada
+                  // O pop() true entra no then e podemos exercutar ou criar a função que quisermos
+                  Provider.of<Products>(context, listen: false)
+                      .deleteProduct(product.id);
+                });
               },
             ),
           ],
